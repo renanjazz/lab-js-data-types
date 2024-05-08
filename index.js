ec3d76f342354aacda98a40e42e636e0e82c7614
Renan <renan.castro@dashlane.com>
2024-05-08 13:50:52 +0100

    LAB JS data types

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+tongueTwister = s1 + ' ' + s2 + ' ' + s3 + ' ' + s4 + ' ' + s5 + ' ';
 
 // Print out the concatenated string
 
 
+console.log (tongueTwister);
 
 
 /*******************************************
@@ -23,11 +25,15 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const modifiedPart1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const modifiedPart2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
 
 
 
+const result = modifiedPart1 + modifiedPart2;
+console.log(result);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -36,10 +42,12 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+let tipAmount = (billTotal * 15) / 100;
 
 // Print out the tipAmount
 
 
+console.log(tipAmount);
 
 
 /*******************************************
@@ -48,10 +56,12 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const num = Math.round(Math.random() * 10);
 
 // Print the generated random number
 
 
+console.log(num);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +84,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+// false
+// true
+// false
+// true
+// true
+// false
+// true
